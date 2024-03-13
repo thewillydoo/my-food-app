07b218f5d0a2d42783a2ad14cab092fa1f082b07
William Luo <william2299@gmail.com>
2024-03-13 06:39:01 -0600

    Add Notification, Navbar, and Footer components to layout

---
+++ src/components/Menu.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from "next/image";
+
+const Menu = () => {
+  return (
+    <div>
+      <Image
+        src="/open.png"
+        alt=""
+        width={20}
+        height={20}/>
+    </div>
+  )
+}
+
+export default Menu

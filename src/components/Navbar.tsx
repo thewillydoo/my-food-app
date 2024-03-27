4bfc628ddf609ff7e6f25fa838438ea1675f2155
William Luo <william2299@gmail.com>
2024-03-27 13:30:53 -0600

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Menu from './Menu'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div>
       {/*LOGO*/}
+      <div>
+        <Link href="/">
+        Willy's Pizza Creations
+        </Link>
+      </div>
       {/*MOBILE MENU*/}
       <div>
         <Menu/>

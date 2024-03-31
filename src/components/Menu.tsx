76d5070724ef9ba47c549038356716e2bcbc2270
William Luo <william2299@gmail.com>
2024-03-31 01:57:23 -0600

    Add open/close functionality to Menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,17 +1,34 @@
 "use client"
 
-import React from 'react'
+import  React, { useState } from 'react'
 import Image from "next/image";
+
+const links = [
+  {id: 1, title: "Homepage", url: "/"},
+  {id: 2, title: "Menu", url: "/menu"},
+  {id: 3, title: "Working Hours", url: "/"},
+  {id: 4, title: "Contact", url: "/"},
+]
 
 const Menu = () => {
   const [open, setOpen] = useState(false)
   return (
     <div>
+       {!open ?
       <Image
         src="/open.png"
         alt=""
         width={20}
-        height={20}/>
+        height={20}
+        onClick={()=>setOpen(true)}/>
+        :
+      <Image
+        src="/close.png"
+        alt=""
+        width={20}
+        height={20}
+        onClick={()=>setOpen(false)}/>
+       }
     </div>
   )
 }

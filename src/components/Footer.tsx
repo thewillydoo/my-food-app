7bbb68b725e73671ccc2b0688f1d70cdcbeb3b06
William Luo <william2299@gmail.com>
2024-04-02 00:22:45 -0600

    Refactor page.tsx, Footer.tsx, and Navbar.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='h-12 text-red-500 p-4 flex items-center justify-between uppercase md:h-24 lg:p-20 xl:p-40'>
+      <Link href='/' className='font-bold text-xl'>VIVA GUSTO GRILLE</Link>
+      <p>© 2024 All rights reserved.</p>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,15 +7,15 @@ import Image from "next/image";
 const Navbar = () => {
   const user = false;
   return (
-    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24">
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
       {/*LEFT LINKS*/}
-      <div className="hidden md:flex gap-4">
+      <div className="hidden md:flex gap-4 flex-1">
         <Link href="/">Homepage</Link>
         <Link href="/menu">Menu</Link>
         <Link href="/">Contact</Link>
       </div>
       {/*LOGO*/}
-      <div className="text-xl">
+      <div className="text-xl md:font-bold flex-1 md:text-center">
         <Link href="/">Viva Gusto Grille</Link>
       </div>
       {/*MOBILE MENU*/}
@@ -23,8 +23,8 @@ const Navbar = () => {
         <Menu />            
       </div>
       {/*RIGHT LINKS*/}
-      <div className="hidden md:flex gap-4 items-center">
-        <div className="flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md: absolute top-3 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
           <Image src="/phone.png" alt="" width={20} height={20}/>
           <span>123 456 7890</span> 
         </div>

2cbe1b68ec9ccdf67d32d96802307d9b7dfbaed9
William Luo <william2299@gmail.com>
2024-04-05 02:16:49 -0600

    Refactor Featured component layout and styles

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -7,17 +7,17 @@ const Featured = () => {
       {/* WRAPPER */}
       <div className='w-max flex'>
         {/* SINGLE ITEM */}
-        <div className='w-screen h-[60vh] flex flex-col items-center justify-around'>
+        <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4'>
           {/* IMAGE CONTAINER */}
           <div className='relative flex-1 w-full'>
-            <Image src='/temporary/p1.png' alt='' fill/>
+            <Image src='/temporary/p1.png' alt='' fill className='object-contain'/>
           </div>
           {/* TEXT CONTAINER  */}
-          <div className=''>
-            <h1>title</h1>
+          <div className='flex-1 flex flex-col gap-4'>
+            <h1 className='text-xl font-bold uppercase'>title</h1>
             <p>description</p>
-            <span>Price</span>
-            <button>Add to Cart</button>
+            <span className='text-xl font-bold'>$123.99</span>
+            <button className='bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
           </div>
         </div>
       </div>

800a2d54b561cd5c3b1536c2def55a09d1f2b2c4
William Luo <william2299@gmail.com>
2024-04-07 04:46:11 -0600

    Add dynamic menu links

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,9 +1,14 @@
+import { menu } from '@/data'
+import Link from 'next/link'
 import React from 'react'
 
 const MenuPage = () => {
   return (
     <div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
-      MenuPage
+      {menu.map((category) => (
+        <Link href={category.slug}>
+        </Link>
+      ))}
     </div>
   )
 }

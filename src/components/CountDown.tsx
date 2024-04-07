dce973ea03fb2601c85437308b05b87a5267e1ac
William Luo <william2299@gmail.com>
2024-04-07 03:15:21 -0600

    Update dependencies and add countdown component

---
+++ src/components/CountDown.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const CountDown = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CountDown
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
+import CountDown from './CountDown'
 
 const Offer = () => {
   return (
     <div className='bg-black h-screen flex flex-col md:flex-row'>
       {/* TEXT CONTAINER */}  
-      <div className='flex-1'>
+      <div className='flex-1 flex flex-col justify-center items-center text-center gap-8 p-6'>
         <h1 className='text-white text-5xl font-bold xl:6xl'>Delicious Burger & French Fry</h1>
-        <p className='text-white'>Progressively simplify effective e-toilers and process-centric methods
+        <p className='text-white xl:text-xl'>Progressively simplify effective e-toilers and process-centric methods
           of empowerment. Quickly pontificate parallel.
         </p>
-        <button className=''>Order Now</button>
+        <CountDown/>
+        <button className='bg-red-500 text-white rounded-md py-3 px-6'>Order Now</button>
       </div>
       {/* IMAGE CONTAINER */}  
       <div className='flex-1 w-full relative'>

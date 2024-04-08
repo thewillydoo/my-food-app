d97e12ae65d413795f4ac780133a3495cc720432
William Luo <william2299@gmail.com>
2024-04-07 19:25:33 -0600

    Refactor menu page layout and styles

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -7,14 +7,14 @@ const MenuPage = () => {
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
         <Link
-          href={category.slug}
-          className=""
+          href={`/menu/${category.slug}`}
+          className="w-full h-1/3 bg-cover p-8 md:h-1/2 "
           style={{ backgroundImage: `url(${category.img})` }} 
         >
-          <div className="">
-            <h1 className="">{category.title}</h1>
-            <p className="">{category.desc}</p>
-            <button className="">Explore</button>
+          <div className={`text-${category.color} w-1/2`}>
+            <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
+            <p className="text-sm my-8">{category.desc}</p>
+            <button className={`hidden 2xl:block bg-black text-white py-2 px-4 rounded-md`}>Explore</button>
           </div>
         </Link>
       ))}
